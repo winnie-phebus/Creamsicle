4022ac01b1d6315b3edabc8445cddb89400df159
winnie.phebus <winn.phebus@gmail.com>
2020-07-08 23:33:04 -0400

    welcome screen and settings BEGIN

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// TODO: figure out how to make constant
+ThemeData kOrangeThemeData = ThemeData(
+  primarySwatch: Colors.orange,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
