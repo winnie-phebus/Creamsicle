ece2efbab87c67b14b60db3c4e6a29cf09507dce
winnie.phebus <winn.phebus@gmail.com>
2020-07-21 00:42:45 -0400

    head work

--- lib/model/Character.dart
+++ lib/model/Character.dart
@@ -1,4 +1,4 @@
-// TODO: document this!!
+// TODO: document this!! Look into using DartDoc.
 import 'dart:ui';
 
 import 'package:creamsicle/model/enums.dart';
@@ -7,57 +7,121 @@ import 'package:creamsicle/model/enums.dart';
 // TODO: also, don't forget to TEST the whole model!
 //  Want to make sure she works, no?
 class Character {
+  // Character Info / Bio Things
   String name;
   Sex bodyType;
   Info charInfo;
 
-  Head _head;
-  Body _body;
-  Outfit _clothes;
+  // Character Visual things
+  Head head;
+  Body body;
+  Outfit clothes;
 
-  // sets the sex of the given character
-  void setSex(Sex given) {
-    this.bodyType = given;
-  }
-
-  static Character randomize() {
-    // TODO: add all the randomize
-    return Character();
-  }
+  // Constructor
+  Character(this.name, this.bodyType, this.charInfo, this.head, this.body,
+      this.clothes);
 }
 
 // TODO: go through and finish all these inner classes, get all the way down to individual Features!!
 class Head {
+  // HAIR
   Hair bangs;
   Hair behind;
 
-  static Head randomize() {
-    return Head();
-  }
+  // FACE
+  Brows eyebrows;
+  Eyes eyes;
+  Nose nose;
+  Mouth mouth;
+
+  // GENERAL
+  Ears ears;
+  FaceShape faceShape;
+
+  // Constructor
+  Head(this.bangs, this.behind, this.eyebrows, this.eyes, this.nose, this.mouth,
+      this.ears, this.faceShape);
+
+// TODO: maybe randomize should belong to the View Classes / interpretations?
 }
 
-class Hair {}
+class Ears extends Feature {
+  // Constructor
+  Ears(description) : super(description, Layer.mid);
+}
+
+class Hair extends Feature {
+  // Constructor
+  Hair(description, stackLayer) : super(description, stackLayer);
+}
+
+class Brows extends FacialFeature {
+  Brows(description) : super(description, Expression.neutral);
+}
+
+class Eyes extends FacialFeature {
+  Eyes(description) : super(description, Expression.neutral);
+}
+
+class Nose extends Feature {
+  Nose(description) : super(description, Layer.above);
+}
+
+class Mouth extends FacialFeature {
+  Mouth(description) : super(description, Expression.neutral);
+}
 
 class Body {
   static Body randomize() {
     return Body();
   }
+
+  Body();
 }
 
-class Outfit {}
+class Outfit {
+  Outfit();
+}
 
 class Info {
   int age;
   String birthday;
   Zodiac sign;
   String bio;
+
+  Info(this.age, this.birthday, this.sign, this.bio);
 }
 
-class CharacterPart {
-  Color skincolor;
+class BodyPart {
+  Color skinColor;
+  Thickness currentThickness;
+
+  // Default Constructor
+  BodyPart();
+
+  BodyPart.instance(this.skinColor, this.currentThickness);
 }
 
-abstract class Feature {
+abstract class Feature extends BodyPart {
   // used to identify what visual component should be summoned
   String description;
+  Layer stackLayer;
+
+  // Constructor
+  Feature(this.description, this.stackLayer);
 }
+
+abstract class FacialFeature extends Feature {
+  // used to find which version of the facial feature should currently be shown.
+  Expression currentExpression;
+
+  // Constructor
+  FacialFeature(description, this.currentExpression)
+      : super(description, Layer.above);
+}
+
+abstract class ClothingItem {
+  List<Tag> tags;
+}
+
+class Tag {}
--- lib/model/enums.dart
+++ lib/model/enums.dart
@@ -1,10 +1,11 @@
-/// This file contains the various enums used throughout the model that builds each Character.
+/// This file contains the enums used throughout the model that builds a Character.
 
-// Sex refers to the biological sex of the Character,
-// defaults to female because of Cream
+/// Sex refers to the biological sex of the Character,
+///
+/// defaults to female because of Cream.
 enum Sex { female, male, intersex }
 
-// Zodiac is the traditional 12 Zodiac Signs from Astrology
+/// Zodiac is the traditional 12 Zodiac Signs from Astrology.
 enum Zodiac {
   Aries,
   Taurus,
@@ -19,3 +20,20 @@ enum Zodiac {
   Aquarius,
   Pisces,
 }
+
+/// Represents the different states of a FacialFeature.
+enum Expression { neutral, angry, happy, sad }
+
+// TODO: maybe refactor? Used to be Weight
+enum Thickness { slim, average, large }
+
+/// Represents what the face shape of the Character currently is.
+enum FaceShape { diamond, heart, round, oval, square, oblong }
+
+/// Helps tell the View where each component is in the stack.
+///
+/// In order, [lowest] is for the way back of a Character and holds things like
+/// wings or tails. Then, [below] is for the back-hair, [mid] is for skin & like
+/// body parts, [above] is for the facial features and [high] is for bangs.
+/// Clothing may also be on this structure, or may have their own.
+enum Layer { lowest, below, mid, above, high }
